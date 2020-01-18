f25ae335c0d7ea386fade931b5af95802024cda6
mei23 <m@m544.net>
2020-01-18 15:54:36 +0900

    tune

--- src/server/index.ts
+++ src/server/index.ts
@@ -19,14 +19,9 @@ app.use(async ctx => {
 		ctx.body = summary;
 		ctx.set('Cache-Control', 'public, max-age=3600');
 	} catch (e) {
-		if (e.statusCode >= 400 && e.statusCode < 500) {
-			ctx.status = e.statusCode;
-			ctx.set('Cache-Control', 'public, max-age=3600');
-		} else {
-			console.log(`summaly error: ${e} ${ctx.query.url}`);
-			ctx.status = 500;
-			ctx.set('Cache-Control', 'public, max-age=300');
-		}
+		console.log(`summaly error: ${e} ${ctx.query.url}`);
+		ctx.status = 500;
+		ctx.set('Cache-Control', 'public, max-age=3600');
 	}
 });
 

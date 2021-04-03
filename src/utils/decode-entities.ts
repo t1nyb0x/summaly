c849ef98cb077976b122deba248a38e82a19896a
mei23 <m@m544.net>
2021-04-04 00:57:50 +0900

    html-entities@2.3.2

--- src/utils/decode-entities.ts
+++ src/utils/decode-entities.ts
@@ -1,9 +1,8 @@
 import clip from './clip';
-import { AllHtmlEntities } from 'html-entities';
-const entities = new AllHtmlEntities();
+import { decode } from 'html-entities';
 
 export function decodeEntities(description: string | null | undefined, limit = 300): string | null {
 	return description
-		? (clip(entities.decode(description), limit) || null)
+		? (clip(decode(description), limit) || null)
 		: null;
 }

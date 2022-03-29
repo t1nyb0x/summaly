4c8e0f67e279083901e9b0c8184320e28409c0fb
mei23 <m@m544.net>
2022-03-30 02:47:34 +0900

    Use MozJPEG

--- src/utils/image-processor.ts
+++ src/utils/image-processor.ts
@@ -19,7 +19,8 @@ export async function ConvertToJpeg(path: string, width: number, height: number)
 		.rotate()
 		.jpeg({
 			quality: 85,
-			progressive: true
+			progressive: true,
+			mozjpeg: true,
 		})
 		.toBuffer();
 
@@ -27,49 +28,5 @@ export async function ConvertToJpeg(path: string, width: number, height: number)
 		data,
 		ext: 'jpg',
 		type: 'image/jpeg'
-	};
-}
-
-/**
- * Convert to WebP
- *   with resize, remove metadata, resolve orientation, stop animation
- */
-export async function ConvertToWebp(path: string, width: number, height: number): Promise<IImage> {
-	const data = await sharp(path)
-		.resize(width, height, {
-			fit: 'inside',
-			withoutEnlargement: true
-		})
-		.rotate()
-		.webp({
-			quality: 85
-		})
-		.toBuffer();
-
-	return {
-		data,
-		ext: 'webp',
-		type: 'image/webp'
-	};
-}
-
-/**
- * Convert to PNG
- *   with resize, remove metadata, resolve orientation, stop animation
- */
-export async function ConvertToPng(path: string, width: number, height: number): Promise<IImage> {
-	const data = await sharp(path)
-		.resize(width, height, {
-			fit: 'inside',
-			withoutEnlargement: true
-		})
-		.rotate()
-		.png()
-		.toBuffer();
-
-	return {
-		data,
-		ext: 'png',
-		type: 'image/png'
 	};
 }

97d0f0c7994e28be1f760771fe3d2b4fba89d5c5
MeiMei <30769358+mei23@users.noreply.github.com>
2023-12-25 01:43:09 +0900

    Resolve #691 (#692)
    * Resolve #691
    
    * a
    
    * at
    
    * at2

--- built/general.js
+++ built/general.js
@@ -13,58 +13,29 @@ const cleanup_title_1 = require("./utils/cleanup-title");
 const decode_entities_1 = require("./utils/decode-entities");
 const got_1 = require("./utils/got");
 exports.default = (url, lang = null) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l, _m, _o, _p, _q, _r, _s, _t, _u, _v, _w, _x, _y, _z, _0, _1, _2, _3, _4, _5, _6;
     if (lang && !lang.match(/^[\w-]+(\s*,\s*[\w-]+)*$/))
         lang = null;
     const res = yield (0, got_1.scpaping)(url.href, { lang: lang || undefined });
     const $ = res.$;
     const landingUrl = new URL(res.response.url);
-    const twitterCard = $('meta[property="twitter:card"]').attr('content');
-    let title = $('meta[property="og:title"]').attr('content') ||
-        $('meta[property="twitter:title"]').attr('content') ||
-        $('title').text() ||
-        null;
-    if (title == null) {
-        throw 'no title';
-    }
+    const twitterCard = (_a = $('meta[name="twitter:card"]').attr('content')) !== null && _a !== void 0 ? _a : $('meta[property="twitter:card"]').attr('content');
+    let title = (_e = (_d = (_c = (_b = $('meta[name="twitter:title"]').attr('content')) !== null && _b !== void 0 ? _b : $('meta[property="twitter:title"]').attr('content')) !== null && _c !== void 0 ? _c : $('meta[property="og:title"]').attr('content')) !== null && _d !== void 0 ? _d : $('title').text()) !== null && _e !== void 0 ? _e : null;
     title = (0, decode_entities_1.decodeEntities)(title, 300);
-    let image = $('meta[property="og:image"]').attr('content') ||
-        $('meta[property="twitter:image"]').attr('content') ||
-        $('link[rel="image_src"]').attr('href') ||
-        $('link[rel="apple-touch-icon"]').attr('href') ||
-        $('link[rel="apple-touch-icon image_src"]').attr('href') ||
-        null;
+    let image = (_l = (_k = (_j = (_h = (_g = (_f = $('meta[name="twitter:image"]').attr('content')) !== null && _f !== void 0 ? _f : $('meta[property="twitter:image"]').attr('content')) !== null && _g !== void 0 ? _g : $('meta[property="og:image"]').attr('content')) !== null && _h !== void 0 ? _h : $('link[rel="image_src"]').attr('href')) !== null && _j !== void 0 ? _j : $('link[rel="apple-touch-icon"]').attr('href')) !== null && _k !== void 0 ? _k : $('link[rel="apple-touch-icon image_src"]').attr('href')) !== null && _l !== void 0 ? _l : null;
     image = image ? new URL(image, landingUrl.href).href : null;
-    let playerUrl = (twitterCard !== 'summary_large_image' && $('meta[property="twitter:player"]').attr('content')) ||
-        (twitterCard !== 'summary_large_image' && $('meta[name="twitter:player"]').attr('content')) ||
-        $('meta[property="og:video"]').attr('content') ||
-        $('meta[property="og:video:secure_url"]').attr('content') ||
-        $('meta[property="og:video:url"]').attr('content') ||
-        null;
+    let playerUrl = (_r = (_q = (_p = (_o = (_m = (twitterCard !== 'summary_large_image' && $('meta[name="twitter:player"]').attr('content'))) !== null && _m !== void 0 ? _m : (twitterCard !== 'summary_large_image' && $('meta[property="twitter:player"]').attr('content'))) !== null && _o !== void 0 ? _o : $('meta[property="og:video"]').attr('content')) !== null && _p !== void 0 ? _p : $('meta[property="og:video:secure_url"]').attr('content')) !== null && _q !== void 0 ? _q : $('meta[property="og:video:url"]').attr('content')) !== null && _r !== void 0 ? _r : null;
     playerUrl = playerUrl ? new URL(playerUrl, landingUrl.href).href : null;
-    const playerWidth = parseInt($('meta[property="twitter:player:width"]').attr('content') ||
-        $('meta[name="twitter:player:width"]').attr('content') ||
-        $('meta[property="og:video:width"]').attr('content') ||
-        '');
-    const playerHeight = parseInt($('meta[property="twitter:player:height"]').attr('content') ||
-        $('meta[name="twitter:player:height"]').attr('content') ||
-        $('meta[property="og:video:height"]').attr('content') ||
-        '');
-    let description = $('meta[property="og:description"]').attr('content') ||
-        $('meta[property="twitter:description"]').attr('content') ||
-        $('meta[name="description"]').attr('content') ||
-        null;
+    const playerWidth = parseInt((_u = (_t = (_s = $('meta[name="twitter:player:width"]').attr('content')) !== null && _s !== void 0 ? _s : $('meta[property="twitter:player:width"]').attr('content')) !== null && _t !== void 0 ? _t : $('meta[property="og:video:width"]').attr('content')) !== null && _u !== void 0 ? _u : '');
+    const playerHeight = parseInt((_x = (_w = (_v = $('meta[name="twitter:player:height"]').attr('content')) !== null && _v !== void 0 ? _v : $('meta[property="twitter:player:height"]').attr('content')) !== null && _w !== void 0 ? _w : $('meta[property="og:video:height"]').attr('content')) !== null && _x !== void 0 ? _x : '');
+    let description = (_1 = (_0 = (_z = (_y = $('meta[name="twitter:description"]').attr('content')) !== null && _y !== void 0 ? _y : $('meta[property="twitter:description"]').attr('content')) !== null && _z !== void 0 ? _z : $('meta[property="og:description"]').attr('content')) !== null && _0 !== void 0 ? _0 : $('meta[name="description"]').attr('content')) !== null && _1 !== void 0 ? _1 : null;
     description = (0, decode_entities_1.decodeEntities)(description, 300);
     if (title === description) {
         description = null;
     }
-    let siteName = $('meta[property="og:site_name"]').attr('content') ||
-        $('meta[name="application-name"]').attr('content') ||
-        landingUrl.hostname ||
-        null;
+    let siteName = (_4 = (_3 = (_2 = $('meta[property="og:site_name"]').attr('content')) !== null && _2 !== void 0 ? _2 : $('meta[name="application-name"]').attr('content')) !== null && _3 !== void 0 ? _3 : landingUrl.hostname) !== null && _4 !== void 0 ? _4 : null;
     siteName = (0, decode_entities_1.decodeEntities)(siteName, 300);
-    const favicon = $('link[rel="shortcut icon"]').attr('href') ||
-        $('link[rel="icon"]').attr('href') ||
-        '/favicon.ico';
+    const favicon = (_6 = (_5 = $('link[rel="shortcut icon"]').attr('href')) !== null && _5 !== void 0 ? _5 : $('link[rel="icon"]').attr('href')) !== null && _6 !== void 0 ? _6 : '/favicon.ico';
     const icon = favicon ? new URL(favicon, landingUrl.href).href : null;
     const sensitive = $('.tweet').attr('data-possibly-sensitive') === 'true';
     // Clean up the title
--- src/general.ts
+++ src/general.ts
@@ -10,55 +10,56 @@ export default async (url: URL, lang: string | null = null): Promise<SummalyEx> 
 	const $ = res.$;
 	const landingUrl = new URL(res.response.url);
 
-	const twitterCard = $('meta[property="twitter:card"]').attr('content');
+	const twitterCard =
+		$('meta[name="twitter:card"]').attr('content') ??
+		$('meta[property="twitter:card"]').attr('content');
 
-	let title =
-		$('meta[property="og:title"]').attr('content') ||
-		$('meta[property="twitter:title"]').attr('content') ||
-		$('title').text() ||
+	let title: string | null =
+		$('meta[name="twitter:title"]').attr('content') ??
+		$('meta[property="twitter:title"]').attr('content') ??
+		$('meta[property="og:title"]').attr('content') ??
+		$('title').text() ??
 		null;
-
-	if (title == null) {
-		throw 'no title';
-	}
-
 	title = decodeEntities(title, 300);
 
 	let image =
-		$('meta[property="og:image"]').attr('content') ||
-		$('meta[property="twitter:image"]').attr('content') ||
-		$('link[rel="image_src"]').attr('href') ||
-		$('link[rel="apple-touch-icon"]').attr('href') ||
-		$('link[rel="apple-touch-icon image_src"]').attr('href') ||
+		$('meta[name="twitter:image"]').attr('content') ??
+		$('meta[property="twitter:image"]').attr('content') ??
+		$('meta[property="og:image"]').attr('content') ??
+		$('link[rel="image_src"]').attr('href') ??
+		$('link[rel="apple-touch-icon"]').attr('href') ??
+		$('link[rel="apple-touch-icon image_src"]').attr('href') ??
 		null;
 
 	image = image ?  new URL(image, landingUrl.href).href : null;
 
 	let playerUrl =
-		(twitterCard !== 'summary_large_image' && $('meta[property="twitter:player"]').attr('content')) ||
-		(twitterCard !== 'summary_large_image' && $('meta[name="twitter:player"]').attr('content')) ||
-		$('meta[property="og:video"]').attr('content') ||
-		$('meta[property="og:video:secure_url"]').attr('content') ||
-		$('meta[property="og:video:url"]').attr('content') ||
+		(twitterCard !== 'summary_large_image' && $('meta[name="twitter:player"]').attr('content')) ??
+		(twitterCard !== 'summary_large_image' && $('meta[property="twitter:player"]').attr('content')) ??
+		$('meta[property="og:video"]').attr('content') ??
+		$('meta[property="og:video:secure_url"]').attr('content') ??
+		$('meta[property="og:video:url"]').attr('content') ??
 		null;
+
 	playerUrl = playerUrl ? new URL(playerUrl, landingUrl.href).href : null;
 
 	const playerWidth = parseInt(
-		$('meta[property="twitter:player:width"]').attr('content') ||
-		$('meta[name="twitter:player:width"]').attr('content') ||
-		$('meta[property="og:video:width"]').attr('content') ||
+		$('meta[name="twitter:player:width"]').attr('content') ??
+		$('meta[property="twitter:player:width"]').attr('content') ??
+		$('meta[property="og:video:width"]').attr('content') ??
 		'');
 
 	const playerHeight = parseInt(
-		$('meta[property="twitter:player:height"]').attr('content') ||
-		$('meta[name="twitter:player:height"]').attr('content') ||
-		$('meta[property="og:video:height"]').attr('content') ||
+		$('meta[name="twitter:player:height"]').attr('content') ??
+		$('meta[property="twitter:player:height"]').attr('content') ??
+		$('meta[property="og:video:height"]').attr('content') ??
 		'');
 
 	let description =
-		$('meta[property="og:description"]').attr('content') ||
-		$('meta[property="twitter:description"]').attr('content') ||
-		$('meta[name="description"]').attr('content') ||
+		$('meta[name="twitter:description"]').attr('content') ??
+		$('meta[property="twitter:description"]').attr('content') ??
+		$('meta[property="og:description"]').attr('content') ??
+		$('meta[name="description"]').attr('content') ??
 		null;
 
 	description = decodeEntities(description, 300);
@@ -67,17 +68,17 @@ export default async (url: URL, lang: string | null = null): Promise<SummalyEx> 
 		description = null;
 	}
 
-	let siteName =
-		$('meta[property="og:site_name"]').attr('content') ||
-		$('meta[name="application-name"]').attr('content') ||
-		landingUrl.hostname ||
+	let siteName: string | null =
+		$('meta[property="og:site_name"]').attr('content') ??
+		$('meta[name="application-name"]').attr('content') ??
+		landingUrl.hostname ??
 		null;
 
 	siteName = decodeEntities(siteName, 300);
 
 	const favicon =
-		$('link[rel="shortcut icon"]').attr('href') ||
-		$('link[rel="icon"]').attr('href') ||
+		$('link[rel="shortcut icon"]').attr('href') ??
+		$('link[rel="icon"]').attr('href') ??
 		'/favicon.ico';
 
 	const icon = favicon ? new URL(favicon, landingUrl.href).href : null;

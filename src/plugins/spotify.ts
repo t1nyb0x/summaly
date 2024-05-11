285f8f2b29371c2fafcd573d955ff0c0b88a42c9
t1nyb0x <dev.t1nyb0x@gmail.com>
2024-05-12 01:54:17 +0900

    fix spotify.link scrape

--- src/plugins/spotify.ts
+++ src/plugins/spotify.ts
@@ -1,30 +1,40 @@
 import { getJson } from '../utils/got';
 import { Summaly } from '../summaly';
 import * as cheerio from 'cheerio';
-import general from '../general';
+import { scpaping } from '../utils/got';
 
 export function test(url: URL): boolean {
 	if (url.hostname === 'open.spotify.com' || url.hostname === 'spotify.link' || url.hostname === 'spotify.app.link') {
 		return true;
 	}
-
 	return false;
 }
 
-export async function process(url: URL): Promise<Summaly> {
-	// get summary
-	const originalUrl = new URL(url.href);
-	if (url.hostname === 'spotify.link') url.hostname = 'spotify.app.link';
-	const summary = await general(url);
-	originalUrl.href = summary.url;
+export async function process(url: URL, lang: string | null = null, useRange = false): Promise<Summaly> {
+	if (url.hostname.indexOf('spotify.link') === 0 || url.hostname.indexOf('spotify.app.link') === 0) {
+		if (url.hostname === 'spotify.link') {
+			url.hostname = 'spotify.app.link';
+		}
 
-	// build oEmbed url
+		const scrapingResult = await scpaping(url.href, { lang: lang || undefined, useRange });
+
+		if (!scrapingResult.$) {
+			throw new Error('Scraping failed');
+		}
+
+		let openSpotifyUrl = scrapingResult.$('a.secondary-action').attr('href');
+		if (!openSpotifyUrl) {
+			openSpotifyUrl = url.href;
+		}
+
+		url.href = openSpotifyUrl;
+	}
+	
 	const oEmbedUrl = new URL('https://open.spotify.com/oembed');
-	oEmbedUrl.searchParams.append('url', originalUrl.href);
-
-	// get oEmbed
+	oEmbedUrl.searchParams.append('url', url.href);
+	
 	const oEmbedResponse = await getJson(oEmbedUrl.href, 'https://spotify.com') as OEmbed;
-	// parse
+	
 	const $ = cheerio.load(oEmbedResponse.html);
 
 	const playerUrl = $('iframe').attr('src');
@@ -32,7 +42,7 @@ export async function process(url: URL): Promise<Summaly> {
 
 	return {
 		title: oEmbedResponse.title ?? null,
-		description: summary.description,
+		description: oEmbedResponse.description ?? null,
 		icon: 'https://open.spotifycdn.com/cdn/images/favicon32.b64ecc03.png',
 		sitename: oEmbedResponse.provider_name ?? null,
 		thumbnail: oEmbedResponse.thumbnail_url ?? null,
@@ -41,7 +51,7 @@ export async function process(url: URL): Promise<Summaly> {
 			width: oEmbedResponse.width,
 			height: oEmbedResponse.height,
 		},
-		url: originalUrl.href,
+		url: url.href,
 	};
 }
 
@@ -53,7 +63,8 @@ type OEmbed = {
 	author_url?: string;
 	provider_name?: string;
 	provider_url?: string;
-	cache_age?: number; // in sec
+	cache_age?: number;
+	description?: string;
 	thumbnail_url?: string;
 	thumbnail_height: number,
 	thumbnail_width: number,
